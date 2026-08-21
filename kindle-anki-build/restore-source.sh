528183c44e8d5ff87431d59c94f0df4880f98e98
melspixel <melquiades13141@gmail.com>
2026-08-21 17:50:54 +0800

    Fix lossless Kindle Anki source restoration

--- kindle-anki-build/restore-source.sh
+++ kindle-anki-build/restore-source.sh
@@ -2,7 +2,7 @@
 set -eu
 HERE=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 cd "$HERE"
-base64 --decode source.b64 > source.zip
+cat src.part-* | base64 --decode > source.zip
 sha256sum -c source.sha256
 rm -rf kindle-anki-port
 unzip -q source.zip
